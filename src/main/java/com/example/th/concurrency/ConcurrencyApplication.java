a3628c8ce6c59328a78f64803e92ef762c73a56d
admin <498649238@qq.com>
2019-01-11 07:53:56 +0800

    add

---
+++ src/main/java/com/example/th/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,14 @@
+package com.example.th.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+
+}
+
